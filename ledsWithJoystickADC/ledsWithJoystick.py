5952a75c7d71adbf6333d1b92f27c3db71a0de3c
Özgür Bilgen <45699938+Aesgiyr@users.noreply.github.com>
2023-03-24 18:26:02 +0300

    Update ledsWithJoystick.py

--- ledsWithJoystickADC/ledsWithJoystick.py
+++ ledsWithJoystickADC/ledsWithJoystick.py
@@ -1,34 +1,41 @@
 from machine import ADC, Pin
 import time
+
 ledLeft=Pin(0,Pin.OUT,value=0)
 ledMid=Pin(4,Pin.OUT,value=0)
 ledRight=Pin(5,Pin.OUT,value=0)
 ledLeftM=Pin(1,Pin.OUT,value=0)
 ledRightM=Pin(3,Pin.OUT,value=0)
 adc = ADC(0)
+
 while True:
     print(adc.read())
     readValue = adc.read()
+    
     if readValue<540 and readValue>520:
         ledMid.on()
         time.sleep(0.1)
         ledMid.off()
         time.sleep(0.1)
+        
     elif readValue<100:
         ledLeft.on()
         time.sleep(0.1)
         ledLeft.off()
         time.sleep(0.1)
+        
     elif readValue<500:
         ledLeftM.on()
         time.sleep(0.1)
         ledLeftM.off()
         time.sleep(0.1)
+        
     elif readValue>950:
         ledRight.on()
         time.sleep(0.1)
         ledRight.off()
         time.sleep(0.1)
+        
     elif readValue>545:
         ledRightM.on()
         time.sleep(0.1)
